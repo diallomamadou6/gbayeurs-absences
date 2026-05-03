16c13c14c55e8853a86d7eaa4b95ba8ebe9a7c1c
diallomamadou6 <md806765@gmail.com>
2026-05-03 04:28:08 +0000

    Mise à jour pour compatibilité Aiven SSL

--- server/database.js
+++ server/database.js
@@ -7,9 +7,11 @@ dotenv.config();
 // Vous pouvez modifier ces valeurs dans un fichier .env
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'gestion_absences',
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : null,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
